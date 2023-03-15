1cd5450c825feb22ea74643a559fa07cec459fe6
Alex Bodian <113264125+abodian@users.noreply.github.com>
2023-03-15 15:57:33 +0000

    Delete Dictionary.js

--- api/routes/Dictionary.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const DictionaryController = require('../controllers/dictionary')
-
-router.get('/:word', DictionaryController.GetDefinition);
-
-module.exports = router;
