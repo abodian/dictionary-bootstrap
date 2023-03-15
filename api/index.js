acc9fc6d833545aa7749f3a3ea02a5a8204397f8
abodian <ajnbodian@gmail.com>
2023-03-15 15:07:07 +0000

    cors change

--- api/index.js
+++ api/index.js
@@ -13,10 +13,6 @@ app.get("/products/:id", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
-});
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
