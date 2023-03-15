de13d52e485e14372001c2800f27a833c314952e
abodian <ajnbodian@gmail.com>
2023-03-15 15:13:59 +0000

    render cors policy

--- api/index.js
+++ api/index.js
@@ -9,8 +9,16 @@ const app = express();
 
 app.use(cors());
 
-app.get("/products/:id", function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://albo-dictionary-frontend.onrender.com"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 const PORT = process.env.PORT || 4000;
