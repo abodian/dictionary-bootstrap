f34af775a36c27e10dad2e656d61d45f2e45dd94
abodian <ajnbodian@gmail.com>
2023-03-15 15:55:51 +0000

    file name changes

--- api/routes/Dictionary.js
+++ api/routes/Dictionary.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const DictionaryController = require('../controllers/dictionary')
+const DictionaryController = require('../controllers/Dictionary')
 
 router.get('/:word', DictionaryController.GetDefinition);
 
